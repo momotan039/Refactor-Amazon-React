a0c0213f21301cbb74fd2f643ce5a3e8197ee979
momotan <momotaha039@gmail.com>
2023-02-13 16:00:30 +0200

    add viewed by customer section

---
+++ src/components/sections/viewed by customer/ViewedByCustomer.jsx
@@ -0,0 +1,186 @@
+import './ViewedByCustomer.css'
+
+function ViewedByCustomer(){
+    return <>
+    <section className="viewed-by-customer container carousel">
+        <h3 className="title">Customers who viewed this item also viewed</h3>
+        <div className="items">
+            <div className="buttons r-btn">
+                <i className="fas fa-angle-right"></i>
+            </div>
+            <div className="buttons l-btn">
+                <i className="fas fa-angle-left"></i>
+            </div>
+            <div className="product">
+                <a href="#">
+                    <img className="image"
+                        src="https://m.media-amazon.com/images/I/31Q+1u2XFmL._AC_UF320,320_SR320,320_.jpg"/>
+                    <a href="#">SURUIMA GK Vegeta Figure Statues Figurine DBZ Super Saiyan Collection Birthday
+                        Gift...</a>
+                    <div className="stars">
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-halffill.svg" alt="halfstar"/>
+                        7
+                    </div>
+                    $27.89
+                    <p>
+                        Get it as soon as <b>Monday</b>, <b>Jan 30</b>
+                    </p>
+                    <p>
+                        FREE Shipping on orders over $49 shipped by Amazon
+                    </p>
+                </a>
+
+            </div>
+            <div className="product">
+                <a href="#">
+                    <img className="image"
+                        src="https://images-na.ssl-images-amazon.com/images/I/61ZXJJalMhL._AC_UL160_SR160,160_.jpg"/>
+                    <a href="#">LESESOBE Vegeta Figure Statues Figurine DBZ Action Figre Super Saiyan Collection
+                        Birthday Gifts PVC 11 Inch</a>
+                    <div className="stars">
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-halffill.svg" alt="halfstar"/>
+                        7
+                    </div>
+                    $27.89
+                    <p>
+                        Get it as soon as <b>Monday</b>, <b>Jan 30</b>
+                    </p>
+                    <p>
+                        FREE Shipping on orders over $49 shipped by Amazon
+                    </p>
+                </a>
+
+            </div>
+            <div className="product">
+                <a href="#">
+                    <img className="image"
+                        src="https://images-na.ssl-images-amazon.com/images/I/61r32VHL92L._AC_UL160_SR160,160_.jpg"/>
+                    <a href="#">Rikiti Vegeta Figure, DBZ Figure, Buff Vegeta Statue with Dumbbell, Collection Toy
+                        Birthday Gift</a>
+                    <div className="stars">
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-halffill.svg" alt="halfstar"/>
+                        7
+                    </div>
+                    $27.89
+                    <p>
+                        Get it as soon as <b>Monday</b>, <b>Jan 30</b>
+                    </p>
+                    <p>
+                        FREE Shipping on orders over $49 shipped by Amazon
+                    </p>
+                </a>
+
+            </div>
+            <div className="product">
+                <a href="#">
+                    <img className="image"
+                        src="https://images-na.ssl-images-amazon.com/images/I/61gnIO5cy4L._AC_UL160_SR160,160_.jpg"/>
+                    <a href="#">Dragon Ball MAXIMATIC Vegeta II FIG</a>
+                    <div className="stars">
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-halffill.svg" alt="halfstar"/>
+                        7
+                    </div>
+                    $27.89
+                    <p>
+                        Get it as soon as <b>Monday</b>, <b>Jan 30</b>
+                    </p>
+                    <p>
+                        FREE Shipping on orders over $49 shipped by Amazon
+                    </p>
+                </a>
+
+            </div>
+            <div className="product">
+                <a href="#">
+                    <img className="image"
+                        src="https://images-na.ssl-images-amazon.com/images/I/71Q-Zu0Dd3L._AC_UL160_SR160,160_.jpg"/>
+                    <a href="#">Trunks Figure Statues Figurine DBZ Action Figre Super Saiyan Collection Birthday Gifts
+                        PVC 7 Inch</a>
+                    <div className="stars">
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-halffill.svg" alt="halfstar"/>
+                        7
+                    </div>
+                    $27.89
+                    <p>
+                        Get it as soon as <b>Monday</b>, <b>Jan 30</b>
+                    </p>
+                    <p>
+                        FREE Shipping on orders over $49 shipped by Amazon
+                    </p>
+                </a>
+
+            </div>
+            <div className="product">
+                <a href="#">
+                    <img className="image"
+                        src="https://images-na.ssl-images-amazon.com/images/I/51AqMK7YabL._AC_UL160_SR160,160_.jpg"/>
+                    <a href="#">DBZ Self-Destruct Majin Vegeta Collection Figure Figurine Toy Doll 10 inch FASHIONMORE
+                        Green Life</a>
+                    <div className="stars">
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-halffill.svg" alt="halfstar"/>
+                        7
+                    </div>
+                    $27.89
+                    <p>
+                        Get it as soon as <b>Monday</b>, <b>Jan 30</b>
+                    </p>
+                    <p>
+                        FREE Shipping on orders over $49 shipped by Amazon
+                    </p>
+                </a>
+
+            </div>
+            <div className="product">
+                <a href="#">
+                    <img className="image"
+                        src="https://images-na.ssl-images-amazon.com/images/I/5146pqg7GEL._AC_UL160_SR160,160_.jpg"/>
+                    <a href="#">30CM DBZ Jiren Figure Anime Full Power Jiren PVC Action Figures GK Statue Collection
+                        Model Toys for Children FASHIONMORE Green Life</a>
+                    <div className="stars">
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star"/>
+                        <img src="https://m.media-amazon.com/images/G/01/perc/star-halffill.svg" alt="halfstar"/>
+                        7
+                    </div>
+                    $27.89
+                    <p>
+                        Get it as soon as <b>Monday</b>, <b>Jan 30</b>
+                    </p>
+                    <p>
+                        FREE Shipping on orders over $49 shipped by Amazon
+                    </p>
+                </a>
+
+            </div>
+        </div>
+    </section>
+    </>
+}
+
+export default ViewedByCustomer
