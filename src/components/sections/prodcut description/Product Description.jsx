761de7575eea751862fe301a7c265c0819dd93b0
momotan <momotaha039@gmail.com>
2023-02-13 15:54:31 +0200

    add product description

---
+++ src/components/sections/prodcut description/Product Description.jsx
@@ -0,0 +1,42 @@
+import './Product Description.css'
+
+function ProductDesription() {
+    return <>
+        <section className="product-description container">
+            <h3 className="title">Product Description</h3>
+            <p>
+                This toys Vegeta Figure Will bring their favorite characters from the big screen to real life, without
+                leaving the comfort of your home!
+                 <br />
+                This Vegeta Figure utilizing cool style paint operations, and fine details! 
+                <br />
+                Not only does it make an exciting addition to your own toy collection, it is also a fun gift for any
+                occasion. 
+                <br />
+                Even if you’re not a fan yourself, you can grab a set for your children or your friends and see their eyes
+                light up with joy!
+            </p>
+            <p>
+                <b>Commodity material:</b>
+                <br />
+                Made out of Environmental，durable material plastic PVC
+                <br />
+                Size:11"
+                <br />
+                Condition:100% NEW
+            </p>
+            <p>
+                1.All the figures we sell are 100% brand new
+                <br/>
+                    2.Best of all,Our quality is consistent all throughout, from manufacturing and inspections to sales and
+                    post-sales support， Best of all, we offer them at reasonable prices!
+                    <br/>
+                        3.All products we sell checked carefully before shipment,If you have any question, just contact us, we will
+                        do our best to help you.
+                    </p>
+                    <hr />
+                </section>
+            </>
+}
+
+            export default ProductDesription
