f8a97ee84867acc46995fb230185db1eaae6bca5
momotan <momotaha039@gmail.com>
2023-02-13 15:55:07 +0200

    add product detials

---
+++ src/components/sections/product detials/ProductDetials.jsx
@@ -0,0 +1,197 @@
+import './ProductDetials.css'
+function ProdcutDetials() {
+    return <>
+        <section className="product-detials container">
+            <div className="breadcrumb-navigation">
+                <a href="#">Toys & Games</a>
+                <span>-</span>
+                <a href="#">Toy Figures & Playsets</a>
+                <span>-</span>
+                <a href="#">Action Figures</a>
+            </div>
+            <div className="detials">
+                <div className="left">
+                    <div className="sticky">
+                        <div className="pictures">
+                            <img src="https://m.media-amazon.com/images/I/51X-qX9b0OL._AC_Sy550_.jpg" alt="pic1" />
+                            <img src="https://m.media-amazon.com/images/I/41m2zXs3QoL._AC_Sy550_.jpg" alt="pic2" />
+                            <img src="https://m.media-amazon.com/images/I/41yNtyc+xcL._AC_Sy550_.jpg" alt="pic3" />
+                            <img src="https://m.media-amazon.com/images/I/517EhSOrQgL._AC_Sy550_.jpg" alt="pic4" />
+                            <img src="https://m.media-amazon.com/images/I/51WgxanZvDL._AC_Sy550_.jpg" alt="pic5" />
+                            <img src="https://m.media-amazon.com/images/I/51FJNnnwzpL._AC_Sy550_.jpg" alt="pic6" />
+                            <img src="https://m.media-amazon.com/images/I/51G7IFxO99L._AC_Sy550_.jpg" alt="pic7" />
+                        </div>
+                        <div className="picture">
+                            <img src="https://m.media-amazon.com/images/I/71LaXhDzmNL._AC_SX425_.jpg" alt="picture" />
+                            <div className="hint">Roll over image to zoom in</div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="center">
+                    <h2 className="title">
+                        SURUIMA GK Vegeta Figure Statues Figurine DBZ Super Saiyan Collection Birthday Gifts PVC 11Inch
+                    </h2>
+                    <a className="brand" href="#"><span>Brand:SURUIMA GK</span></a>
+                    <div className="rating">
+                        <div className="stars">
+                            <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star" />
+                            <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star" />
+                            <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star" />
+                            <img src="https://m.media-amazon.com/images/G/01/perc/star-fullfill.svg" alt="star" />
+                            <img src="https://m.media-amazon.com/images/G/01/perc/star-halffill.svg" alt="halfstar" />
+                            <span className="arrow"></span>
+                        </div>
+                        <a href="#">6 ratings</a>
+                    </div>
+                    <hr />
+                    <div className="price">
+                        <span className="discount">-16%</span>
+                        <div className="real-price">
+                            <span className="currency">$</span>
+                            <span className="number">32</span>
+                            <span className="sub-number">89</span>
+                        </div>
+                    </div>
+                    <div className="list-price">
+                        List Price: <span>$38.99</span>
+                        <i className="fas fa-info-circle">
+                            <div className="pop-up">
+                                <p>The List Price is the suggested retail price of a new product as provided by a
+                                    manufacturer, supplier, or seller. Except for books, Amazon will display a List Price if
+                                    the product was purchased by customers on Amazon or offered by other retailers at or
+                                    above the List Price in at least the past 90 days. List prices may not necessarily
+                                    reflect the product's prevailing market price.
+                                </p>
+                                <a href="#">Learn more</a>
+                            </div>
+                        </i>
+                    </div>
+                    <div className="fees">
+                        No Import Fees Deposit & $12.53 Shipping to Israel
+                        <a href="#">Details</a> <span className="arrow"></span>
+                    </div>
+                    <div className="coupon">
+                        <span>Coupon:</span>
+                        <a href="#">
+                            <input type="checkbox" />
+                            Apply 5% coupon
+                        </a>
+                    </div>
+                    <div className="terms">
+                        <span>Terms</span>
+                        <span className="arrow"></span>
+                    </div>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Brand</th>
+                                <td>SURUIMA GK</td>
+                            </tr>
+                            <tr>
+                                <th>Material</th>
+                                <td>Plastic, Polyvinyl Chloride</td>
+                            </tr>
+                            <tr>
+                                <th>Occasion</th>
+                                <td>Birthday</td>
+                            </tr>
+                            <tr>
+                                <th>Product Dimensions</th>
+                                <td>3"L x 6"W x 11"H</td>
+                            </tr>
+                            <tr>
+                                <th>Number of Items</th>
+                                <td>1</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <hr />
+                    <div className="about">
+                        <h3>About this item</h3>
+                        <ul>
+                            <li>GREAT ADDITION TO THE COLLECTION： Combine this Vegeta figure , letting the world of DBZ
+                                unfold right on your tabletop !</li>
+                            <li>TWO DIFFERENT HEAD ：Two different head sculpts provide replacement，Simple construction
+                                patterns ensure it will be an easy to build figure even for those who have never built a
+                                model kit before!</li>
+                            <li>GREAT GIFT IDEAS ： A Great Gift Idea For Kids ( girls ) In any occasion !</li>
+                            <li>CLEAN PAINT JOB AND STANCE ： This Vegeta Figure Utilizing cool style sculpting technology to
+                                bring out the coolest possible scenes !
+                            </li>
+                            <li>DESIGNED BY SURUIMA GK ： This product is designed and developed by SURUIMA GK animation
+                                studio, from manufacturing and inspections to sales and post-sales support， Best of all, we
+                                offer them at reasonable prices!
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="data">
+                        <div className="real-price">
+                            <span className="currency">$</span>
+                            <span className="number">32</span>
+                            <span className="sub-number">89</span>
+                        </div>
+                        <div className="fees">
+                            No Import Fees Deposit & $12.53 Shipping to Israel
+                            <a href="#">Details</a> <span className="arrow"></span>
+                        </div>
+                        <div className="delivery">
+                            <div>
+                                FREE delivery <b>Monday, January 30</b> to Israel on $49 of eligible items. Order within
+                                <span className="green">18 hrs 49 mins</span>
+                            </div>
+                            <div>
+                                Or fastest delivery <b>Monday, January 23</b>
+                            </div>
+                            <div className="location">
+                                <i className="fas fa-map-marker-alt"></i>
+                                <a href="#"><span>Deliver to Israel</span></a>
+                            </div>
+                        </div>
+                        <h4 className="status">In Stock</h4>
+                        <select className="qty">
+                            <option selected>Qty: 1</option>
+                            <option>Qty: 2</option>
+                            <option>Qty: 3</option>
+                            <option>Qty: 4</option>
+                            <option>Qty: 5</option>
+                        </select>
+                        <button className="btn yellow">Add to Cart</button>
+                        <button className="btn orange">Buy Now</button>
+                        <div className="secure"><i className="fas fa-lock"></i><a href="#">Secure transaction</a></div>
+                        <div className="ships">
+                            <span>Ships from</span>
+                            <span>Amazon</span>
+                        </div>
+                        <div className="sold">
+                            <span>Sold by</span>
+                            <a href="#">SURUIMA</a>
+                        </div>
+                        <div className="policy">
+                            Return Policy:
+                            <a href="#"> Eligible for Return, Refund or Replacement within 30 days of receipt </a>
+                        </div>
+                        <a href="#">
+                            <input type="checkbox" />
+                            Apply 5% coupon
+                        </a>
+                        <hr />
+                        <button className='add-to-list'>Add to list</button>
+                    </div>
+                    <div className="sell-on-amazon">
+                        <span>Have one to sell?</span>
+                        <button>Sell on Amazon</button>
+                    </div>
+                </div>
+                <div className="scale-image">
+                </div>
+            </div>
+            <hr/>
+        </section>
+    </>
+
+}
+
+export default ProdcutDetials
